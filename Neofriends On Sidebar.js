85b8835b79273b7646eaccd7a279d1d24711701e
pom <35046605+lycheemilk@users.noreply.github.com>
2025-04-08 18:05:12 -0400

    Update Neofriends On Sidebar.js

--- Neofriends On Sidebar.js
+++ Neofriends On Sidebar.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Neofriends On Sidebar
-// @namespace    http://tampermonkey.net/
+// @namespace    neopets
 // @version      1.9
 // @description  Moves Neofriends avatars and makes them clickable, along with the usernames, to the left sidebar with styleable CSS classes.
 // @author       Pomelo
